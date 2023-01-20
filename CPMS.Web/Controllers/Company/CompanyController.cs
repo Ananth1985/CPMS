333ec886db39cd8eadea9f4a60ed05e77fe627b3
Ananth Ganapathy <Ananth.Ganapathy@cudirect.com>
2023-01-20 15:16:46 +0530

    Placement Request Changes

--- CPMS.Web/Controllers/Company/CompanyController.cs
+++ CPMS.Web/Controllers/Company/CompanyController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CPMS.Web.Models;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Security.Policy;
 
 namespace CPMS.Web.Controllers.Company
 {
@@ -6,6 +10,34 @@ namespace CPMS.Web.Controllers.Company
     {
         public IActionResult CompanyCreation()
         {
+            return View();
+        }
+
+        public IActionResult PlacementRequest()
+        {
+            List<string>  collegeList = new List<string>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7128/api");
+
+            // Add an Accept header for JSON format.
+            //client.DefaultRequestHeaders.Accept.Add(
+            //new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync("/College/GetCollegeDetails?collegeId=1").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadFromJsonAsync<CPMS.Web.Models.College[]>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                foreach (var d in dataObjects)
+                {
+                    //Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
             return View();
         }
     }
