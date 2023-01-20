4622c655a092f29634233f4c2b269387d02e3373
Tony Anbarasan <Tony.Anbarasan@rencata.com>
2023-01-19 23:58:40 -0800

    Update namespaces

---
+++ CPMS.Web/Controllers/Company/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPMS.Web.Controllers.Company
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult CompanyCreation()
+        {
+            return View();
+        }
+    }
+}
