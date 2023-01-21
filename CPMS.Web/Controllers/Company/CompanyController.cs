296847c7cab18c3aa3849574b16354dfbd1e6c83
Ananth Ganapathy <Ananth.Ganapathy@cudirect.com>
2023-01-21 16:05:09 +0530

    Placement Request code updated

--- CPMS.Web/Controllers/Company/CompanyController.cs
+++ CPMS.Web/Controllers/Company/CompanyController.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Headers;
 using System.Security.Cryptography.Xml;
 using System.Security.Policy;
 using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CPMS.Web.Controllers.Company
 {
@@ -16,17 +17,24 @@ namespace CPMS.Web.Controllers.Company
 
         public IActionResult PlacementRequest()
         {
-           var list = new List<CPMS.Web.Models.College>();
+            var colleges = new List<CPMS.Web.Models.College>();
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = client.GetAsync("https://localhost:7128/api/College/GetCollegeDetails?collegeId").Result;  
+            List<SelectListItem> collegeList = new List<SelectListItem>();
+            HttpResponseMessage response = client.GetAsync("https://localhost:7128/api/College/GetCollegeDetails?collegeId").Result;
             if (response.IsSuccessStatusCode)
             {
                 var apiResponse = response.Content.ReadAsStringAsync().Result;
-                list = JsonSerializer.Deserialize<List<CPMS.Web.Models.College>>(apiResponse);
+                colleges = JsonSerializer.Deserialize<List<CPMS.Web.Models.College>>(apiResponse);
+                for (int i = 0; i < colleges.Count; i++)
+                {
+                    collegeList.Add(new SelectListItem { Text = colleges[i].CollegeName.ToString(), Value = colleges[i].CollegeId.ToString() });
+                }
+                ViewData["colleges"] = collegeList;
             }
-            return View();
-        }
+                return View();
 
+            
+        }
         
     }
 }
