5f98a04bb40fffea1861605c09730f1bcc13d48b
Ananth Ganapathy <Ananth.Ganapathy@cudirect.com>
2023-01-22 17:09:17 +0530

    Login Screen Update

--- CPMS.Web/Controllers/LoginController.cs
+++ CPMS.Web/Controllers/LoginController.cs
@@ -35,15 +35,19 @@ namespace CPMS.Web.Controllers
             HttpContext.Session.SetString("LoggedInUserEmail", "");
             HttpContext.Session.SetString("LoggedInUserId", "");
             HttpContext.Session.SetString("LoggedInUserType", "");
+            HttpContext.Session.SetString("LoggedInUserProfileId", "");
+            HttpContext.Session.SetString("LoggedInUserConcernName", "");
             return View("Login");
         }
 
         [HttpGet]
-        public void SetSessionItem(string Email,string LoginId,string TypeId)
+        public void SetSessionItem(string Email,string LoginId,string TypeId,string ProfileId,string UserConcernName)
         {
             HttpContext.Session.SetString("LoggedInUserEmail", Email);
             HttpContext.Session.SetString("LoggedInUserId", LoginId);
             HttpContext.Session.SetString("LoggedInUserType", TypeId);
+            HttpContext.Session.SetString("LoggedInUserProfileId", ProfileId);
+            HttpContext.Session.SetString("LoggedInUserConcernName", UserConcernName);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
